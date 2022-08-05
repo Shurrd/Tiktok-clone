be1a72e40ee248d3b7321d057358f7aa2352b620
unknown <engraaadeyemi@gmail.com>
2022-08-05 11:44:01 +0100

    video details, like func, comment func

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,16 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { client } from "../../utils/client";
+import { allUsersQuery } from './../../utils/queries';
+import { client } from '../../utils/client';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    const user = req.body;
-    client
-      .createIfNotExists(user)
-      .then(() => res.status(200).json("Login Success"));
-  }
-}
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const doc = req.body;
+
+  client.createIfNotExists(doc).then(() => {
+    res.status(200).json('Login successful');
+  });
